833adcb8582fe1335780867a2aae539d8b55429e
kwwoo516 <51371085+kwwoo516@users.noreply.github.com>
2022-03-18 10:34:15 +0900

    Delete frontend directory

--- frontend/src/api/covidcenter.js
+++
@@ -1,9 +0,0 @@
-import { covidInstance } from "./index.js";
-
-const covidcenter = covidInstance();
-
-function covidcenterList(params, success, fail) {
-  covidcenter.get(``, { params: params }).then(success).catch(fail);
-}
-
-export { covidcenterList };
